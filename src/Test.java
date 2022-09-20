4cf61852de8679f3c156b6d12bf05923e672efad
Oniqued <zktl3575@gmail.com>
2022-09-20 19:13:43 +0900

    first commit

---
+++ src/Test.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+    public Test() {
+    }
+
+    public static void main(String[] args) {
+        List<String> list;
+        Scanner sc = new Scanner(System.in);
+    }
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
+
